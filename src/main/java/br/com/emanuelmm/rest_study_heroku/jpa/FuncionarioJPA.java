5c7038ac376786e1232322e95bd1a56640280575
emanuelmm <emanuelcordeiro@gmail.com>
2014-12-28 00:49:30 -0300

    trying to use cdi with jetty

---
+++ src/main/java/br/com/emanuelmm/rest_study_heroku/jpa/FuncionarioJPA.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.emanuelmm.rest_study_heroku.jpa;
+
+import br.com.emanuelmm.rest_study_heroku.entity.Funcionario;
+
+/**
+ *
+ * @author emanuelmm
+ */
+public class FuncionarioJPA {
+    
+    public Funcionario buscarPorId(int id) {
+        return new Funcionario();
+    }
+    
+}
--- src/main/java/br/com/emanuelmm/rest_study_heroku/service/PessoalService.java
+++ src/main/java/br/com/emanuelmm/rest_study_heroku/service/PessoalService.java
@@ -1,9 +1,14 @@
 package br.com.emanuelmm.rest_study_heroku.service;
 
+import br.com.emanuelmm.rest_study_heroku.entity.Funcionario;
+import br.com.emanuelmm.rest_study_heroku.jpa.FuncionarioJPA;
 import java.util.Arrays;
 import java.util.List;
+import javax.annotation.ManagedBean;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,12 +17,23 @@ import javax.ws.rs.core.MediaType;
  * @author Emanuel Cordeiro
  */
 @Path("funcionarios")
+@ManagedBean
 public class PessoalService {
-
+    
+    @Inject
+    FuncionarioJPA funcionarioJPA;
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List< String> buscarTodos() {
         return Arrays.asList("Pedro", "Thiago", "João");
     }
-    
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public Funcionario buscarPorId(@PathParam("id") int id) {
+        return funcionarioJPA.buscarPorId(id);
+    }
+
 }
